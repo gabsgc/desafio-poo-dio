59d01f3690aaf443cef803c262d7fa3bd557ba3a
gabsgc <gabriellagarccia@gmail.com>
2023-01-16 17:31:59 -0300

    iniciando projeto e criando classes de dominio

---
+++ src/main/java/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,24 @@
+package br.com.dio.desafio.dominio;
+
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+public class Mentoria extends Conteudo {
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
